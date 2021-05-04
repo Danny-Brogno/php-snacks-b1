648ae80a79ed49b51b1f006b773302f07af44e60
Danny <danny.brogno@gmail.com>
2021-05-04 15:16:53 +0200

    Start

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>LALLERO</title>
+  </head>
+  <body>
+    <style>
+
+      body {
+        background-color: black;
+      }
+
+      header {
+        color: white;
+      }
+
+    </style>
+
+    <!-- Creare un array con 15 numeri casuali, tenendo conto che l'array non dovrà contenere lo stesso numero più di una volta -->
+    <header>
+      <h1>CIAO</h1>
+    </header>
+
+  </body>
+
+</html>
